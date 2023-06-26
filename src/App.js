fd3c397997b3858ebdcbd58a79d624684142eeb3
temishovajumagul <jumagultemishova@gmail.com>
2023-06-26 15:07:47 +0600

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import LessonsCard from './components/UI/LessonsCard'
-
-const lesson = [
-  {
-    img: 'https://www.devopsschool.com/blog/wp-content/uploads/2022/03/java_logo_icon_168609.png',
-    title: 'уроки JAVA 5',
-  },
-  {
-    img: 'https://nyesteventuretech.com/images/django-development.png',
-    title: 'уроки DJANGO 5',
-  },
-  {
-    img: 'https://miro.medium.com/v2/resize:fit:2000/1*6ahbWjp_g9hqhaTDSJOL1Q.png',
-    title: 'уроки JS 5',
-  },
-]
 function App() {
-  return (
-    <div>
-      <LessonsCard group={lesson} />
-    </div>
-  )
+  return <div>siuuu</div>
 }
 
 export default App
